e7e2f82671bde889d14ff188b09d0eb72da3b416
dirquel <rodrilitoyt@gmail.com>
2023-05-06 15:01:36 +0200

    added languagen to guildDelete event

--- eventos/guildDelete.js
+++ eventos/guildDelete.js
@@ -20,5 +20,5 @@ module.exports = async (client, guild) => {
 
     // Notificación de antiguo gremio.
     await client.channels.fetch(process.env.BOT_PRIVATE_LOGS);
-    client.channels.cache.get(process.env.BOT_PRIVATE_LOGS).send({ embeds: [ new Discord.MessageEmbed().setThumbnail(`${guild.iconURL()}`).setTitle('Me han expulsado de un servidor.').addField('Servidor', `${guild.name} (${guild.id})`).addField('Region', `${guild.region}`).addField('Roles', `${guild.roles.cache.size}`).addField('Miembros', `${guild.memberCount}`).setTimestamp().setColor(0x0056ff).setFooter(`${guild.name}`, `${guild.iconURL()}`) ] });
+    client.channels.cache.get(process.env.BOT_PRIVATE_LOGS).send({ embeds: [ new Discord.MessageEmbed().setThumbnail(`${guild.iconURL()}`).setTitle('Me han expulsado de un servidor.').addField('Servidor', `${guild.name} (${guild.id})`).addField('Idioma', `${guild.preferredLocale}`).addField('Roles', `${guild.roles.cache.size}`).addField('Miembros', `${guild.memberCount}`).setTimestamp().setColor(0x0056ff).setFooter(`${guild.name}`, `${guild.iconURL()}`) ] });
 }
