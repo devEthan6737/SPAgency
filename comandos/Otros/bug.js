7ac804b62dd993b95e5b8e593650d042e04651ac
dirquel <rodrilitoyt@gmail.com>
2023-05-07 13:59:11 +0200

    fixed bug command

--- comandos/Otros/bug.js
+++ comandos/Otros/bug.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js-light');
 const antiRF = require('../../schemas/antiRF_Schema');
 const { dataRequired, fecthUsersDataBase } = require('../../functions');
 const _bug = new Discord.MessageEmbed().setColor(0x0056ff);
+require('dotenv').config();
 
 module.exports = {
     nombre: "bug",
@@ -20,10 +21,10 @@ module.exports = {
         collector.on('collect', async m => {
             if(m.content == '')return;
             if(m.author.id == message.author.id) {                    
-                if(`${m.content}`.toLowerCase() == 'enviar') {
+                if(`${m.content}`.toLowerCase() == 'enviar') { // esto hay que ver con los lenguajes para que si está en inglés tengas que poner send en vez de enviar, etc.
                     _bug.setDescription(args.join(' ')).setTitle(`${LANG.commands.others.bug.message3}.`);
                     message.reply({ embeds: [ _bug ], ephemeral: true });
-                    client.channels.cache.get('782712586519969792').send({ embeds: [ _bug.setTitle('Bug.').setAuthor(`${message.author.tag}, ${message.author.id}`, message.author.displayAvatarURL()).setFooter(`${message.guild.name}, ${message.guild.id}`, message.guild.iconURL) ] });
+                    client.channels.cache.get(process.env.BOT_PRIVATE_LOGS).send({ embeds: [ _bug.setTitle('Bug.').setAuthor(`${message.author.tag}, ${message.author.id}`, message.author.displayAvatarURL()).setFooter(`${message.guild.name}, ${message.guild.id}`, message.guild.iconURL) ] });
                     collector.stop();
                 }else {
                     message.channel.send({ content: `${LANG.commands.others.bug.message4}.` });
