91d4af0c0720e6d6bfb21054fc277089e01e6996
! Ethan.ᵈᵉᵛ <77934431+devEthan6737@users.noreply.github.com>
2023-05-11 00:59:41 +0200

    Update maliciousRemoved.js

--- eventos/maliciousRemoved.js
+++ eventos/maliciousRemoved.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js-light');
-require('dotenv').config();
 
 module.exports = async (client, malicious) => {
     await client.users.fetch(malicious.userId);
 
-    client.channels.cache.get(process.env.UBFB_LOGS).send({
+    client.channels.cache.get(process.env.BOT_PRIVATE_LOGS).send({
         embeds: [
             new Discord.MessageEmbed().setColor(0x5c4fff).setDescription(`Se ha eliminado un usuario malicioso (**${client.users.cache.get(malicious.userId).tag}**).\nLa prueba de su razón sigue en la base de datos, y se le han agregado antecedentes para mantener el control.\n\n**__Propulsado por UBFB.__**`)
         ]
