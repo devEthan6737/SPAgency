f7cc9a01c2cacfa5a12b8f4ae632c8c043f024cb
dirquel <rodrilitoyt@gmail.com>
2023-05-15 17:39:49 +0200

    channel.js language

--- comandos/Configuracion/channel.js
+++ comandos/Configuracion/channel.js
@@ -8,27 +8,29 @@ module.exports = {
 	description: 'Gestiona los canales de tu servidor de forma más rápida.',
 	usage: ['<prefix>channel {create <channelName>, delete <channelMention>}'],
 	run: async (client, message, args, _guild) => {
+        let LANG = require(`../../LANG/${_guild.configuration.language}.json`);
+
         try{
-            if(!message.guild.me.permissions.has('ADMINISTRATOR')) return message.reply({ content: 'Necesito permiso de __Administrador__.', ephemeral: true });
-            if(!message.member.permissions.has('MANAGE_CHANNELS'))return message.reply({ content: 'Necesitas permisos de __Gestionar Canales__.', ephemeral: true });
-            if(!args[0])return message.reply(await dataRequired('No has especificado la función del comando.\n\n' + _guild.configuration.prefix + 'channel {create <channelName>, delete <channelMention>}'));
+            if(!message.guild.me.permissions.has('ADMINISTRATOR')) return message.reply({ content: LANG.data.permissionsADMINme, ephemeral: true });
+            if(!message.member.permissions.has('MANAGE_CHANNELS'))return message.reply({ content: LANG.data.permissionsChannelsU, ephemeral: true });
+            if(!args[0])return message.reply(await dataRequired(LANG.commands.config.channel.message1 + _guild.configuration.prefix + LANG.commands.config.channel.message2));
 
             if(args[0] == 'create') {
-                if(!args[1])return message.reply(await dataRequired('No has escrito el nombre del nuevo canal.\n\n' + _guild.configuration.prefix + 'channel create <channelName>'));
+                if(!args[1])return message.reply(await dataRequired(LANG.commands.config.channel.message3 + _guild.configuration.prefix + LANG.commands.config.channel.message4));
                 message.guild.channels.create(args.join(' ').split('create')[1]);
-                message.reply({ content: 'Canal creado.', ephemeral: true });
+                message.reply({ content: LANG.commands.config.channel.message5, ephemeral: true });
             }else if(args[0] == 'delete') {
                 let channelMention = message.mentions.channels.first();
-                if(!channelMention)return message.reply(await dataRequired('Necesitas mencionar un canal.\n\n' + _guild.configuration.prefix + 'channel delete <channelMention>'));
+                if(!channelMention)return message.reply(await dataRequired(LANG.commands.config.channel.message6 + _guild.configuration.prefix + LANG.commands.config.channel.message7));
                 if(message.guild.channels.cache.has(channelMention.id)) {
-                    if(!channelMention.parentId)return message.reply('`Error 003`: Channel must be on this guild.');
+                    if(!channelMention.parentId)return message.reply(LANG.errors.e3);
                     channelMention.delete().catch(err => {});
-                    message.reply({ content: 'Canal borrado.', ephemeral: true });
+                    message.reply({ content: LANG.commands.config.channel.message8, ephemeral: true });
                 }else{
-                    message.reply(await dataRequired('¡El canal mencionado debe estar en este servidor!\n\n' + _guild.configuration.prefix + 'delete <channelMention>'));
+                    message.reply(await dataRequired(LANG.commands.config.channel.message9 + _guild.configuration.prefix + LANG.commands.config.channel.message7));
                 }
             }else{
-                message.reply(await dataRequired('¡Esa función no es válida!\n\n' + _guild.configuration.prefix + 'channel {create <channelName>, delete <channelMention>}'));
+                message.reply(await dataRequired(LANG.commands.config.channel.message10 + _guild.configuration.prefix + LANG.commands.config.channel.message2));
             }
         }catch(err) {}
 	},
