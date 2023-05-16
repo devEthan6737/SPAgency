c7607e391a1709f6233780ba91734d9b7a625f4b
dirquel <rodrilitoyt@gmail.com>
2023-05-16 18:48:15 +0200

    predict language

--- comandos/Moderacion/predict.js
+++ comandos/Moderacion/predict.js
@@ -18,7 +18,9 @@ module.exports = {
 	description: 'Predice si una imagen o gif es NSFW o no.',
 	usage: ['<prefix>predict'],
 	run: async (client, message, args, _guild) => {
-        if(message.attachments.size == 0)return message.reply('Debes adjuntar una imagen.');
+        let LANG = require(`../../LANG/${_guild.configuration.language}.json`);
+
+        if(message.attachments.size == 0)return message.reply({ content: LANG.commands.mod.predict.message1});
         let ping = Date.now();
         message.attachments.forEach(async x => {
             if(x.proxyURL.endsWith('png') || x.proxyURL.endsWith('gif') || x.proxyURL.endsWith('jpeg') || x.proxyURL.endsWith('jpg') || x.proxyURL.endsWith('bmp')) {
@@ -35,11 +37,11 @@ module.exports = {
                 setTimeout(async () => {
                     let response = await getResponseAndDelete(message.author.id);
                     message.reply({ embeds: [
-                        new Discord.MessageEmbed().setDescription(`Respuesta en **${ms(Date.now() - ping)}**\n\n${response.predictions.map(predict => `\`func ${predict[0]}\` __says__ **${alt(predict[1])}**`).join('\n')}`).setColor('RED')
+                        new Discord.MessageEmbed().setDescription(`${LANG.commands.mod.predict.message2} **${ms(Date.now() - ping)}**\n\n${response.predictions.map(predict => `\`func ${predict[0]}\` __says__ **${alt(predict[1])}**`).join('\n')}`).setColor('RED')
                     ] });
                 }, 4000);
             }else{
-                message.reply('`Solo se admiten formatos PNG, GIF, JPG, BMP.`');
+                message.reply({ content: LANG.commands.mod.predict.message3});
             }
         });
 	},
