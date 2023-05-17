634a88dd1bef52e96183250582222679c10641a3
dirquel <rodrilitoyt@gmail.com>
2023-05-17 19:15:36 +0200

    empban language

--- comandos/Moderacion/tempban.js
+++ comandos/Moderacion/tempban.js
@@ -11,35 +11,37 @@ module.exports = {
 	description: 'Banea a un usuario de tu servidor.',
 	usage: ['<prefix>ban <userMention> <timeout> [reason]'],
 	run: async (client, message, args, _guild) => {
-		if(!message.guild.me.permissions.has('BAN_MEMBERS')) return message.channel.send('Necesito permiso de __Banear Miembros__.');
-		if(!message.member.permissions.has('BAN_MEMBERS')) return message.channel.send('Necesitas permiso de __Banear Miembros__.');
+        let LANG = require(`../../LANG/${_guild.configuration.language}.json`);
+
+		if(!message.guild.me.permissions.has('BAN_MEMBERS')) return message.channel.send({ content: LANG.data.permissionsBanMe });
+		if(!message.member.permissions.has('BAN_MEMBERS')) return message.channel.send({ content: LANG.data.permissionsBan });
 
 		let userMention = message.mentions.members.first();
-        if(!userMention)return message.reply(await dataRequired('Debes mencionar al usuario que deseas banear.\n\n' + _guild.configuration.prefix + 'tempban <userMention> <timeout> [reason]'));
+        if(!userMention)return message.reply(await dataRequired(LANG.commands.mod.tempban.message1 + _guild.configuration.prefix + LANG.commands.mod.tempban.message2));
         if(userMention.id == client.user.id)return;
-		if(userMention.id == message.author.id)return message.reply('No.');
-		if(message.member.roles.highest.comparePositionTo(userMention.roles.highest) <= 0)return message.reply('La persona tiene un rol más alto que tú o tiene el mismo rol.');
+		if(userMention.id == message.author.id)return message.reply({ content: LANG.commands.mod.tempban.message3 });
+		if(message.member.roles.highest.comparePositionTo(userMention.roles.highest) <= 0)return message.reply({ content: LANG.commands.mod.tempban.message3});
 
-        if(!args[1])return message.reply(await dataRequired('Debes escribir el tiempo que estará baneado el usuario.\n\n' + _guild.configuration.prefix + 'tempban <userMention> <timeout> [reason]'));
+        if(!args[1])return message.reply(await dataRequired(LANG.commands.mod.tempban.message5 + _guild.configuration.prefix + LANG.commands.mod.tempban.message2));
         let time = ms(args[1]);
-        if(!time)return message.reply('`Error 006`: No time typed.');
+        if(!time)return message.reply({ content: LANG.commands.mod.tempban.message6 });
         if(time < 120000) {
             time = 12000;
             args[1] = '2m';
         }
 
         if(_guild.moderation.dataModeration.forceReasons.length > 0) {
-            if(!args[2])return message.reply(await dataRequired('El servidor tiene razones forzadas activas, es necesario adjuntar una razón en la sanción.\n\n' + _guild.configuration.prefix + 'tempban <userMention> <timeout> <reason>\n\nRazones forzadas: ' + _guild.moderation.dataModeration.forceReasons.map(x => `${x}`).join(', ')));
-            if(!_guild.moderation.dataModeration.forceReasons.includes(args[2]))return message.reply(await dataRequired('Esa razón no es válida, el servidor tiene razones forzadas activas.\n\n' + _guild.configuration.prefix + 'tempban <userMention> <reason>\n\nRazones forzadas: ' + _guild.moderation.dataModeration.forceReasons.map(x => `${x}`).join(', ')));
+            if(!args[2])return message.reply(await dataRequired(LANG.commands.mod.tempban.message7 + _guild.configuration.prefix + LANG.commands.mod.tempban.message8 + _guild.moderation.dataModeration.forceReasons.map(x => `${x}`).join(', ')));
+            if(!_guild.moderation.dataModeration.forceReasons.includes(args[2]))return message.reply(await dataRequired(LANG.commands.mod.tempban.message9 + _guild.configuration.prefix + LANG.commands.mod.tempban.message8 + _guild.moderation.dataModeration.forceReasons.map(x => `${x}`).join(', ')));
         }
-        if(!args[2]) args[2] = 'Sin especificar.';
+        if(!args[2]) args[2] = LANG.commands.mod.tempban.message10;
 
-        if(!userMention.bannable) return message.reply('No puedo banear al usuario mencionado.');
+        if(!userMention.bannable) return message.reply({ content: LANG.commands.mod.tempban.message11 });
 		let userID = client.users.cache.get(userMention.id);
-		userID.send('Has sido baneado temporalmente de `' + message.guild.name + '` durante `' + args[1] + '`.\n\n**Moderador:** `' + message.author.tag + '`\n**__Razón:__** `' + args.join(' ').split(`${args[1]} `)[1] + '`').then(x => {
+		userID.send(LANG.commands.mod.tempban.message12 + message.guild.name + LANG.commands.mod.tempban.message13 + args[1] + '\n' + LANG.commands.mod.tempban.message14 + message.author.tag + LANG.commands.mod.tempban.message15 + args.join(' ').split(`${args[1]} `)[1] + '`').then(x => {
             userMention.ban({ reason: args.join(' ').split(`${args[1]} `)[1] });
             let banEmbed = new Discord.MessageEmbed()
-			.setDescription(`**__Miembro baneado:__** <@${userMention.id}> (${userMention.id})\n**__Moderador:__** <@${message.author.id}> (${message.author.id})\n**__Razón:__** \`${args.join(' ').split(`${args[1]} `)[1]}\`\n**__Duración del baneo:__** \`${args[1]}\``)
+			.setDescription(`${LANG.commands.mod.tempban.message16} <@${userMention.id}> (${userMention.id})${LANG.commands.mod.tempban.message14} <@${message.author.id}> (${message.author.id})${LANG.commands.mod.tempban.message15} \`${args.join(' ').split(`${args[1]} `)[1]}\`\n${LANG.commands.mod.tempban.message17} \`${args[1]}\``)
 			.setTimestamp().setColor(0x5c4fff);
             message.channel.send({ embeds: [ banEmbed ] });
         }).catch(err => {});
