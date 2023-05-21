2e943369129bdba303f81f9bddc1eb41d73ff6a9
VirtualOx <73899696+VirtualOx-sys@users.noreply.github.com>
2023-05-21 17:04:07 -0500

    DisVerse Bot Page Link Changed

--- comandos/Otros/invite.js
+++ comandos/Otros/invite.js
@@ -8,6 +8,6 @@ module.exports = {
     run: async (client, message, args, _guild) => {
         let LANG = require(`../../LANG/${_guild.configuration.language}.json`);
 
-        message.reply({ content: '' + LANG.commands.others.invite.message1 + '\n\n https://disverse.space/bot/1038614901394002020', ephemeral: true });
+        message.reply({ content: '' + LANG.commands.others.invite.message1 + '\n\nhttps://disverse.space/bot/1038614901394002020', ephemeral: true });
     }
 }
