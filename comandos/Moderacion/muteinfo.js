448d6a0705805942682ce819b5e7abda82b61dbc
dirquel <rodrilitoyt@gmail.com>
2023-06-16 23:24:48 +0200

    muteinfo language

--- comandos/Moderacion/muteinfo.js
+++ comandos/Moderacion/muteinfo.js
@@ -9,28 +9,31 @@ module.exports = {
 	description: 'Obtén información sobre una persona muteada o todos los usuarios muteados.',
 	usage: ['<prefix>muteinfo [userMention]'],
 	run: async (client, message, args, _guild) => {
-		if(!message.guild.me.permissions.has('MANAGE_ROLES')) return message.channel.send('Necesito permiso de __Gestionar Roles__.');
-		if(!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send('Necesitas permiso de __Expulsar Miembros__.');
-        if(!_guild.moderation.dataModeration.muterole)return message.channel.send(`Se debe especificar el rol de muteo con \`${_guild.configuration.prefix}setmuterole <roleMention>\``);
+
+        let LANG = require(`../../LANG/${_guild.configuration.language}.json`);
+
+		if(!message.guild.me.permissions.has('MANAGE_ROLES')) return message.channel.send({ content: LANG.data.permissionsRolesme });
+		if(!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send({ content: LANG.data.permissionsKick });
+        if(!_guild.moderation.dataModeration.muterole)return message.channel.send({content: LANG.commands.mod.muteinfo.message1 + _guild.configuration.prefix + LANG.commands.mod.muteinfo.message2 });
 
 		let userMention = message.mentions.members.first();
         if(!userMention) {
-            if(_guild.moderation.dataModeration.timers.length > 20)return message.reply({ content: `Hay \`${_guild.moderation.dataModeration.timers.length}\` usuarios muteados.`, components: [
+            if(_guild.moderation.dataModeration.timers.length > 20)return message.reply({ content: LANG.commands.mod.muteinfo.message3 + _guild.moderation.dataModeration.timers.length + LANG.commands.mod.muteinfo.message4, components: [
                 new Discord.MessageActionRow()
                 .addComponents(new Discord.MessageButton()
-                    .setLabel('Desmutear a todos.')
+                    .setLabel(LANG.commands.mod.muteinfo.message5)
                     .setEmoji('🛑')
                     .setCustomId('unmuteAll')
                     .setStyle('DANGER'),
                 )
             ] });
             let cc = 1;
-            message.reply({ content: `Hay \`${_guild.moderation.dataModeration.timers.length}\` usuarios muteados.`, embeds: [ new Discord.MessageEmbed().setColor(0x0056ff)
-                .setAuthor('Usuarios muteados:').setDescription(`${_guild.moderation.dataModeration.timers.map(x => `\`${cc++}.\` ${x.user.username}`).join('\n')}`)
+            message.reply({ content: LANG.commands.mod.muteinfo.message3 + _guild.moderation.dataModeration.timers.length + LANG.commands.mod.muteinfo.message4, embeds: [ new Discord.MessageEmbed().setColor(0x0056ff)
+                .setAuthor(LANG.commands.mod.muteinfo.message6).setDescription(`${_guild.moderation.dataModeration.timers.map(x => `\`${cc++}.\` ${x.user.username}`).join('\n')}`)
             ], components: [
                 new Discord.MessageActionRow()
                     .addComponents(new Discord.MessageButton()
-                        .setLabel('Desmutear a todos.')
+                        .setLabel(LANG.commands.mod.muteinfo.message5)
                         .setEmoji('🛑')
                         .setCustomId('unmuteAll')
                         .setStyle('DANGER'),
@@ -38,8 +41,8 @@ module.exports = {
             ] });
         }else{
             if(userMention.id == client.user.id)return;
-            if(userMention.id == message.author.id)return message.reply('Es estuvieses muteado, ¿cómo escribes el comando?.');
-            if(!userMention.roles.cache.has(_guild.moderation.dataModeration.muterole))return message.channel.send('Beep, beep. El usuario no está muteado.');
+            if(userMention.id == message.author.id)return message.reply({ content: LANG.commands.mod.muteinfo.message7 });
+            if(!userMention.roles.cache.has(_guild.moderation.dataModeration.muterole))return message.channel.send({ content: LANG.commands.mod.muteinfo.message8});
 
             let has = false;
             _guild.moderation.dataModeration.timers.forEach(x => {
@@ -47,11 +50,11 @@ module.exports = {
                     has = true;
                     message.reply({ embeds: [
                         new Discord.MessageEmbed().setColor(0x0056ff).setAuthor(`${userMention.user.tag}`, `${userMention.user.displayAvatarURL()}`)
-                        .setDescription(`Roles almacenados: \`${x.user.roles.length} roles\`\nDesmuteo programado para: \`${new Date(x.endAt)}, dentro de ${ms(x.endAt - Date.now())}\`\nFue muteado en el canal: \`${x.channel}\``)
+                        .setDescription(LANG.commands.mod.muteinfo.message9 + x.user.roles.length + LANG.commands.mod.muteinfo.message10 + new Date(x.endAt) + LANG.commands.mod.muteinfo.message11 + ms(x.endAt - Date.now()) + LANG.commands.mod.muteinfo.message12 + x.channel + '`')
                     ] });
                 }
             });
-            if(has == false)return message.channel.send({ content: 'El usuario está muteado de por vida.' });
+            if(has == false)return message.channel.send({ content: LANG.commands.mod.muteinfo.message13 });
         }
 	},
 };
