18374985d505a5e9c9e257f84ee14704b1814303
! Ethan.ᵈᵉᵛ <77934431+devEthan6737@users.noreply.github.com>
2023-06-22 12:30:04 +0200

    Update unnuke.js

--- comandos/Configuracion/unnuke.js
+++ comandos/Configuracion/unnuke.js
@@ -10,12 +10,14 @@ module.exports = {
 	description: 'Un destructor automático de canales raideados.',
 	usage: ['<prefix>unnuke {channels, roles, emojis, bans}'],
 	run: async (client, message, args, _guild) => {
-        if(!message.guild.me.permissions.has('ADMINISTRATOR'))return message.reply({ content: 'Necesitas permisos de __Administrador__.', ephemeral: true });
-        if(!message.member.permissions.has('ADMINISTRATOR'))return message.reply({ content: 'Necesitas permisos de __Administrador__.', ephemeral: true });
+        let LANG = require(`../../LANG/${_guild.configuration.language}.json`);
+
+        if(!message.guild.me.permissions.has('ADMINISTRATOR'))return message.channel.send(`${LANG.data.permissionsADMINme}.`);
+        if(!message.member.permissions.has('ADMINISTRATOR'))return message.channel.send(`${LANG.data.permissionsADMIN}.`);
 
         if(cooldown.has(message.author.id)) {
             let c = await cooldown.get(message.author.id);
-            if(c > Date.now())return message.channel.send({ content: 'Debes esperar 15 minutos para usar de nuevo el comando.' });
+            if(c > Date.now())return message.channel.send(LANG.commands.config.unnuke.message1);
             cooldown.delete(message.author.id);
         }
 
@@ -23,7 +25,7 @@ module.exports = {
     
         if(args[0] == 'channels') {
             cooldown.set(message.author.id, Date.now() + ms('15m'));
-            message.reply({ content: 'El servidor está siendo analizado y corregido (Puede demorarse esta acción), si falta algo podrá hacerlo de forma manual.' });
+            message.reply(LANG.commands.config.unnuke.message2);
             let _channels = [];
             message.guild.channels.cache.forEach(x => {
                 if(_channels.includes(x.name)) {
@@ -43,7 +45,7 @@ module.exports = {
             });
         }else if(args[0] == 'roles') {
             cooldown.set(message.author.id, Date.now() + ms('15m'));
-            message.reply({ content: 'El servidor está siendo analizado y corregido (Puede demorarse esta acción), si falta algo podrá hacerlo de forma manual.' });
+            message.reply(LANG.commands.config.unnuke.message2);
             let _roles = [];
             message.guild.roles.cache.forEach(x => {
                 contador++;
@@ -63,7 +65,7 @@ module.exports = {
             });
         }else if(args[0] == 'emojis') {
             cooldown.set(message.author.id, Date.now() + ms('15m'));
-            message.reply({ content: 'El servidor está siendo analizado y corregido (Puede demorarse esta acción), si falta algo podrá hacerlo de forma manual.' });
+            message.reply(LANG.commands.config.unnuke.message2);
             let _emojis = [];
             message.guild.emojis.cache.forEach(x => {
                 contador++;
@@ -83,7 +85,7 @@ module.exports = {
             });
         }else if(args[0] == 'bans') {
             cooldown.set(message.author.id, Date.now() + ms('15m'));
-            message.reply({ content: 'El servidor está siendo analizado y corregido (Puede demorarse esta acción), si falta algo podrá hacerlo de forma manual.' });
+            message.reply(LANG.commands.config.unnuke.message2);
             let arr = await message.guild.bans.fetch();
     
             arr.forEach(x => {
@@ -93,7 +95,7 @@ module.exports = {
                 }, 2000 * contador);
             });
         }else{
-            message.reply(await dataRequired(`No se ha escrito la función del comando.\n\n${_guild.configuration.prefix}unnuke {channels, roles, emojis, bans}`));
+            message.reply(await dataRequired(`${LANG.commands.config.unnuke.message3}\n\n${_guild.configuration.prefix}unnuke {channels, roles, emojis, bans}`));
         }
 	},
 };
