40039aa3a6de38ef4f6159edffa6a0bada636573
! Ethan.ᵈᵉᵛ <77934431+devEthan6737@users.noreply.github.com>
2023-07-25 02:06:46 +0200

    Update sugerencia.js

--- comandos/Otros/sugerencia.js
+++ comandos/Otros/sugerencia.js
@@ -10,20 +10,22 @@ module.exports = {
     description: "¿Alguna sugerencia sobre la agencia?",
     usage: ['<prefix>sugerencia <message>'],
     run: async (client, message, args, _guild) => {
-        if(!args[0])return message.reply(await dataRequired('No has especificado el mensaje de la sugerencia.\n\n' + _guild.configuration.prefix + 'sugerencia <message>'));
+        let LANG = require(`../../LANG/${_guild.configuration.language}.json`);
 
-        message.channel.send({ content: 'Estás a punto de enviar ese mensaje a los agentes de SP Agency (Quiere decir que el personal de la agencia lo leerá).\n\nPara continuar escribe `enviar`' });
+        if(!args[0])return message.reply(LANG.commands.others.sugerencia.message1 + await dataRequired('\n\n' + _guild.configuration.prefix + 'sugerencia <message>'));
+
+        message.channel.send(LANG.commands.others.sugerencia.message2);
         let collector = message.channel.createMessageCollector({ time: 15000 });
         collector.on('collect', async m => {
             if(m.content == '')return;
             if(m.author.id == message.author.id) {                    
                 if(`${m.content}`.toLowerCase() == 'enviar') {
-                    _sugerencia.setDescription(args.join(' ')).setTitle('Sugerencia enviada al soporte de SPA.');
+                    _sugerencia.setDescription(args.join(' ')).setTitle(LANG.commands.others.sugerencia.message3);
                     message.reply({ embeds: [ _sugerencia ], ephemeral: true });
-                    client.channels.cache.get(process.env.BOT_PRIVATE_LOGS).send({ embeds: [ _sugerencia.setTitle('Sugerencia.').setAuthor(`${message.author.tag}, ${message.author.id}`, message.author.displayAvatarURL()).setFooter(`${message.guild.name}, ${message.guild.id}`, message.guild.iconURL) ] });
+                    client.channels.cache.get(process.env.BOT_PRIVATE_LOGS).send({ embeds: [ _sugerencia.setTitle(LANG.commands.others.sugerencia.message4).setAuthor(`${message.author.tag}, ${message.author.id}`, message.author.displayAvatarURL()).setFooter(`${message.guild.name}, ${message.guild.id}`, message.guild.iconURL) ] });
                     collector.stop();
                 }else {
-                    message.channel.send({ content: 'Entendido, he detenido el comando.' });
+                    message.channel.send(LANG.commands.others.sugerencia.message5);
                     collector.stop();
                 }
             }
