579b06937ea25abe6a7c58dcf38a759c05f9b99d
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-08-21 00:31:38 +0200

    fix(antiraid): migrar fetchAuditLogs type 'CREATE_CHANNEL' -> AuditLogEvent.ChannelCreate
    eventos/channelCreate.js. Verificado node --check y existencia real
    del simbolo AuditLogEvent.ChannelCreate.

--- eventos/channelCreate.js
+++ eventos/channelCreate.js
@@ -7,7 +7,7 @@ module.exports = async (client, channel) => {
 
     let LANG = require(`../LANG/${_guild.configuration.language}.json`);
 
-    channel.guild.fetchAuditLogs({ type: 'CREATE_CHANNEL' }).then(async logs => {
+    channel.guild.fetchAuditLogs({ type: Discord.AuditLogEvent.ChannelCreate }).then(async logs => {
         let prsn = logs.entries.first().executor;
 
         // Logs:
